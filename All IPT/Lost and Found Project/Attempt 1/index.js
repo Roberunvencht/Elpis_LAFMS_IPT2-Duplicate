d5f8ff809e481d708292cf8b5668d6e075a93f76
herqodess <2301101895@student.buksu.edu.ph>
2025-09-05 11:56:16 +0800

    Backend file

--- All IPT/Lost and Found Project/Attempt 1/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('toggle-sidebar').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('sidebar-closed');
-}); 
